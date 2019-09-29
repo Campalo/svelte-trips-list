0fd195920d3667e1d4949ffd8528bde317a6faed
Campalo <25295489+Campalo@users.noreply.github.com>
2019-09-29 13:48:44 +0200

    add style to Trip

--- src/Trip.svelte
+++ src/Trip.svelte
@@ -1,7 +1,18 @@
+<style>
+  section {
+    padding: 1rem;
+    margin: 0.5rem;
+    background-color: aliceblue;
+    border-radius: 8px;
+    box-shadow: 2px 2px 5px 0px rgba(0, 0, 0, 0.4);
+    color: dimgrey;
+  }
+</style>
+
 <section>
-  <h2>Destination</h2>
-  <h3>Duration</h3>
-  <h3>Date</h3>
-  <h4>Participants</h4>
-  <p>Details</p>
+  <h2>Destination:</h2>
+  <h3>Duration: days</h3>
+  <h3>Date:</h3>
+  <h4>Participants:</h4>
+  <p>Details:</p>
 </section>
